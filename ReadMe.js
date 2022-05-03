afc4c127cb27a5c2a1250a96821279d3293d4c5d
Ben Smith <benlsmith329@gmail.com>
2022-05-03 16:50:07 -0400

    added const and table of contents

---
+++ ReadMe.js
@@ -0,0 +1,17 @@
+const generateReadme = (answers, githubInfo) => {
+        return `
+    <h1>${answers.projectName}</h1>
+    <p>${answers.description}</p>
+    <h2>Table of Contents</h2>
+    <ul>
+        <li><a href="#Installation">Installation</a></li>
+        <li><a href="#Usage">Usage</a></li>
+        <li><a href="#License">License</a></li>
+        <li><a href="#Contributing">Contributing</a></li>
+        <li><a href="#Tests">Tests</a></li>
+        <li><a href="#Questions">Questions</a></li>
+    </ul>
+    
+    <
+
+}
