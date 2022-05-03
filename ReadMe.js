d46244c798de44b15d6f67f7b77f9cd1ecd1c8d9
Ben Smith <benlsmith329@gmail.com>
2022-05-03 17:05:41 -0400

    finished ReadMe.js

--- ReadMe.js
+++ ReadMe.js
@@ -12,6 +12,25 @@ const generateReadme = (answers, githubInfo) => {
         <li><a href="#Questions">Questions</a></li>
     </ul>
     
-    <
+    <h2 id="Installation">Installation</h2>
+    <p>${answers.install}</p>
+    <h2 id="Usage">Usage</h2>
+    <p>${answers.usage}</p>
+    <h2 id="License">License</hd>
+    <p>Licensed under the<a href="./README.md">${answers.license}</a></p>
+    <h2 id="Contributing">Contributing</h2>
+    <p>${answers.contributing}</p>
+    <h2 id="Tests">Tests</h2>
+    <h3>Follow These Dirrections to Test:</h3>
+    <p>${answers.tests}</p>
+    <h2 id="Questions">Questions</h2>
+
+    <p style="strong"> If you have questions contact ${githubInfo.email}.</p>
+    <img src="${githubInfo.avatar_url}" alt="git hub profile picture" height="225" width="250">
+    <img src="https://img.shields.io/badge/Node-12.16.3-brightgreen">
+    <img src="https://img.shields.io/badge/-JavaScript-brightgreen">
+    <img src="https://img.shields.io/github/followers/denzgrant?label=follow&style=social">
+`;
 
 }
+module.exports = generateReadme;
